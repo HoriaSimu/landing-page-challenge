bf9d422096e90308265af5a438e89a162f98c8b2
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-05-02 22:53:37 +0300

    basic markup faq section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { ReactComponent as FaqImage } from '../assets/images/faq.svg';
 
 const Faq = () => {
+  // to do: function to iterate over an array of text for the questions
+
   return(
-    <div>Test FAQ</div>
+    <section className="faq-section">
+      <div className="faq-top">
+        <div className="faq-text">
+          <h2>FAQ</h2>
+          <h3>Questions and Answers on Professional Traffic Permits:</h3>
+        </div>
+        <div className="faq-image">
+          <FaqImage />
+        </div>
+      </div>
+      <div className="faq-questions">
+        <div className="question-card">
+          <div className="question-card-header">
+            What is a professional traffic permit?
+          </div>
+          <div className="question-card-content">
+            Traffic permits are a requirement for conducting professional traffic.
+          </div>
+        </div>
+      </div>
+      <div className="faq-bottom">
+      </div>
+
+    </section>
   );
 };
 
