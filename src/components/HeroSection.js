4d5c8c01a92ca221d131666b5d5f84b8243001b4
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-05-02 22:14:59 +0300

    basic markup hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../assets/images/logo.svg';
+import { ReactComponent as Mobel } from '../assets/images/mobel.svg';
 
 const HeroSection = () => {
   return(
-    <div>Test Hero section</div>
+    <div>
+      <header className="hero-section">
+        <div className="logo">
+          <Logo />
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">How To</a>
+            </li>
+            <li>
+              <a href="#">FAQs</a>
+            </li>
+            <li>
+              <a href="#" className="contact-button">Contact us</a>
+            </li>
+          </ul>
+        </nav>
+        <h1>Your awesome traffic permit consultant.</h1>
+        <a href="#" className="contact-button">Get started →</a>
+        <Mobel />
+      </header>
+    </div>
   );
 };
 
