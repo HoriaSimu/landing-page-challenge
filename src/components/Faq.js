18347533fc92a1801b09044ec464e8d4a80bf8a5
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-05-04 22:42:27 +0300

    added 'faq' section styling

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -6,6 +6,7 @@ const Faq = () => {
 
   return(
     <section className="faq-section">
+      <div className="faq-background"></div>
       <div className="faq-top">
         <div className="faq-text">
           <h2>FAQ</h2>
@@ -17,15 +18,47 @@ const Faq = () => {
       </div>
       <div className="faq-questions">
         <div className="question-card">
-          <div className="question-card-header">
-            What is a professional traffic permit?
+          <button className="question-card-header">
+            <span className="question-title">What is a professional traffic permit?</span>
+            <span className="question-sign"></span>
+          </button>
+          <div className="question-card-content content-show">
+            Traffic permits are a requirement for conducting professional traffic.
           </div>
+        </div>
+        <div className="question-card">
+          <button className="question-card-header">
+            <span className="question-title">What is a professional traffic permit?</span>
+            <span className="question-sign"></span>
+          </button>
+          <div className="question-card-content">
+            Traffic permits are a requirement for conducting professional traffic.
+          </div>
+        </div>
+        <div className="question-card">
+          <button className="question-card-header">
+            <span className="question-title">Are there professional traffic permit training courses at a distance?</span>
+            <span className="question-sign"></span>
+          </button>
+          <div className="question-card-content">
+            Traffic permits are a requirement for conducting professional traffic.
+          </div>
+        </div>
+        <div className="question-card">
+          <button className="question-card-header">
+            <span className="question-title">What is a professional traffic permit?</span>
+            <span className="question-sign"></span>
+          </button>
           <div className="question-card-content">
             Traffic permits are a requirement for conducting professional traffic.
           </div>
         </div>
       </div>
       <div className="faq-bottom">
+        <button className="load-more-button">
+          <span className="load-more-text">Load More</span>
+          <span className="question-sign"></span>
+        </button>
       </div>
 
     </section>
