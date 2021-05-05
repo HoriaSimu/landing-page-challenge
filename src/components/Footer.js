41ac998a1011952995967232bebcae7afd59e377
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2021-05-05 15:38:54 +0300

    added footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,32 @@ import { ReactComponent as Logo } from '../assets/images/logo.svg';
 import { ReactComponent as FooterCar } from '../assets/images/trekmobil.svg';
 import { ReactComponent as SemicircleForeground } from '../assets/images/semicircle1.svg';
 import { ReactComponent as SemicircleBackground } from '../assets/images/semicircle2.svg';
+import { ReactComponent as IconFB } from '../assets/images/icon-fb.svg';
+import { ReactComponent as IconTwitter } from '../assets/images/icon-t.svg';
+import { ReactComponent as IconInsta } from '../assets/images/icon-insta.svg';
 
 const Footer = () => {
   return(
     <footer>
       <div className="footer-image">
-        <FooterCar />
-        <SemicircleForeground />
-        <SemicircleBackground />
+        <FooterCar className="trekmobil" />
+        <SemicircleForeground className="vector-foreground" />
+        <SemicircleBackground className="vector-background" />
       </div>
       <div className="footer-content">
         <div className="footer-text">
-          We provide traffic management consultants so you get started quickly, contact us for a quote today!
+          <p>We provide traffic management consultants so you get started quickly, contact us for a quote today!</p>
         </div>
         <form onSubmit={(e) => e.preventDefault()}>
           <label>Name</label>
           <input placeholder="Your name" />
           <label>Email address</label>
           <input placeholder="Your email address" />
-          <button>Get started</button>
+          <button className="get-started-button">Get started</button>
         </form>
       </div>
       <div className="footer-bottom">
+        <div className="footer-background"></div>
         <div className="logo-bottom">
           <Logo />
         </div>
@@ -39,11 +43,22 @@ const Footer = () => {
             <li>
               <button>FAQs</button>
             </li>
-            <li>
-              <button>Contact us</button>
-            </li>
           </ul>
         </nav>
+      </div>
+      <div className="footer-copyright">
+          <span>Copyright @ 2019</span>
+          <span className="footer-social-icons">
+            <a href="http://www.facebook.com">
+              <IconFB />
+            </a>
+            <a href="http://www.twitter.com">
+              <IconTwitter />
+            </a>
+            <a href="http://www.instagram.com">
+              <IconInsta />
+            </a>
+          </span>
       </div>
     </footer>
   );
